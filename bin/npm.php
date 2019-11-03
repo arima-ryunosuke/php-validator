0a8954d185f2a33831f4feb45c82b948b639df7e
ryunosuke <ryunosuke.arima@gmail.com>
2019-11-03 21:26:39 +0900

    add project files

---
+++ bin/npm.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once __DIR__ . '/../tests/functions.php';
+
+$npmdir = __DIR__ . '/../npm/' . PHP_OS_FAMILY;
+@mkdir($npmdir, 0777, true);
+$npmdir = realpath($npmdir);
+
+file_put_contents("$npmdir/package.json", json_encode([
+    "name"         => "dummy",
+    "version"      => "1.0.0",
+    "description"  => "dummy",
+    "repository"   => "dummy",
+    "license"      => "MIT",
+    "dependencies" => [
+        "locutus" => "*",
+    ],
+], JSON_PRETTY_PRINT));
+
+set_include_path(get_include_path() . PATH_SEPARATOR . getenv('PATH'));
+
+\ryunosuke\chmonos\process(stream_resolve_include_path('npm'), 'install --no-bin-links', '', $stdout, $stderr, $npmdir);
+echo $stdout;
+echo $stderr;
+\ryunosuke\chmonos\cp_rf("$npmdir/node_modules/locutus/php", dirname(__DIR__) . "/src/template/phpjs/locutus");
