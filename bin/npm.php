bfe5a79c510c51d79117226b017b5518e38d9ce7
ryunosuke <ryunosuke.arima@gmail.com>
2020-09-19 23:37:50 +0900

    [bin] npm コマンドが動いていなかったので修正
    - カレントディレクトリの npm で解決されていた

--- bin/npm.php
+++ bin/npm.php
@@ -17,9 +17,9 @@ file_put_contents("$npmdir/package.json", json_encode([
     ],
 ], JSON_PRETTY_PRINT));
 
-set_include_path(get_include_path() . PATH_SEPARATOR . getenv('PATH'));
+set_include_path(getenv('PATH'));
 
 \ryunosuke\chmonos\process(stream_resolve_include_path('npm'), 'install --no-bin-links', '', $stdout, $stderr, $npmdir);
-echo $stdout;
-echo $stderr;
+fwrite(STDOUT, $stdout);
+fwrite(STDERR, $stderr);
 \ryunosuke\chmonos\cp_rf("$npmdir/node_modules/locutus/php", dirname(__DIR__) . "/src/template/phpjs/locutus");
