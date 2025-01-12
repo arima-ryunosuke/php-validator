2c9281200a569b82ddcdd3f63b14d962a4e6bdac
ryunosuke <ryunosuke.arima@gmail.com>
2025-01-12 14:18:34 +0900

    [feature] js コメントと不要コードの除去
    - <script> は大昔にコード補完を効かせるために書いていたもので今は不要

--- src/chmonos/Condition/AbstractCondition.php
+++ src/chmonos/Condition/AbstractCondition.php
@@ -151,13 +151,12 @@ abstract class AbstractCondition
                     $tokens = array_slice(token_get_all("<?php $block"), 1);
                     $tokens = array_map(fn($v) => is_array($v) ? $v : [ord($v), $v], $tokens);
                     $tokens = array_map(fn($v) => $v[0] === T_YIELD ? ['', 'await'] : $v, $tokens);
-                    $tokens = array_filter($tokens, fn($v) => $v[0] !== T_FN);
+                    $tokens = array_filter($tokens, fn($v) => !in_array($v[0], [T_FN, T_COMMENT], true));
                     $block = implode("", array_column($tokens, 1));
                     $vars = array_diff(array_unique(array_column(array_filter($tokens, fn($v) => $v[0] === T_VARIABLE), 1)), $args);
 
                     $code = $class::getJavascriptCode();
                     $code = str_replace('@validationcode:inject', "\n" . $block, $code);
-                    $code = preg_replace('#\s*<script>|</script>\s*#us', '', $code);
                     if ($vars) {
                         $code = 'var ' . implode(', ', $vars) . ";\n" . $code;
                     }
